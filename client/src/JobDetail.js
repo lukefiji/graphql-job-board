58a4c006d429f10b118d1ded2516a1e84684304b
Luke Fiji <luke.fiji@gmail.com>
2018-09-04 19:19:06 -0700

    Finish video 13

--- client/src/JobDetail.js
+++ client/src/JobDetail.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { jobs } from './fake-data';
+import { loadJob } from './requests';
 
 export class JobDetail extends Component {
   constructor(props) {
     super(props);
-    const {jobId} = this.props.match.params;
-    this.state = {job: jobs.find((job) => job.id === jobId)};
+    this.state = { job: null };
+  }
+
+  async componentDidMount() {
+    // Get jobId from React Router route parameter
+    const { jobId } = this.props.match.params;
+    const job = await loadJob(jobId);
+    this.setState({ job });
   }
 
   render() {
-    const {job} = this.state;
+    const { job } = this.state;
+
+    // Return null if job doesn't exist
+    if (!job) {
+      return null;
+    }
+
     return (
       <div>
         <h1 className="title">{job.title}</h1>
--- client/src/requests.js
+++ client/src/requests.js
@@ -24,3 +24,32 @@ export async function loadJobs() {
   const responseBody = await response.json();
   return responseBody.data.jobs;
 }
+
+export async function loadJob(id) {
+  const response = await fetch(endpointURL, {
+    method: 'POST',
+    // Set request content-type to JSON
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({
+      // Send the GraphQL query as an HTTP request
+      // Prefix variables with `$`
+      query: `
+      query JobQuery($id: ID!) {
+        job(id: $id ) {
+          id
+          title
+          company {
+            id
+            name
+          }
+          description
+        }
+      }
+      `,
+      // Pass `id` as a variable into a query
+      variables: { id }
+    })
+  });
+  const responseBody = await response.json();
+  return responseBody.data.job;
+}
